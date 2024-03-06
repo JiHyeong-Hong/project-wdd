0567103d1ec568f60a48b2ca7ea457aacf191d0c
Bam <dhkdndrne@naver.com>
2024-03-06 18:47:41 +0900

    투사체 논타겟 조건 롤백
    화면 밖으로 나가면 무조건 사라지게

--- Assets/Scripts/Contents/Skill/Projectile/Peacock.cs
+++ Assets/Scripts/Contents/Skill/Projectile/Peacock.cs
@@ -40,10 +40,9 @@ public class Peacock : Projectile
 		else
 		{
 			transform.Translate(Vector2.up * (Skill.SkillData.AttackSpeed * Time.deltaTime));
-			
-			if(!Util.CheckTargetInScreen(transform.position))
-				Managers.Object.Despawn(this);
 		}
+		if(!Util.CheckTargetInScreen(transform.position))
+			Managers.Object.Despawn(this);
 	}
 
 	private void OnTriggerEnter2D(Collider2D col)
