114b509cf78ae881a16de7b28ec87306230513e9
kimjiheee <x_by_x@naver.com>
2024-03-07 04:00:16 +0900

    악어 작업중
    발사되면 플레이어가 보는 방향으로 날아가는 것 해야함

--- Assets/Scripts/Contents/Skill/Projectile/Crocodile.cs
+++ Assets/Scripts/Contents/Skill/Projectile/Crocodile.cs
@@ -30,8 +30,6 @@ public class Crocodile : Projectile
         // 랜덤 방향 설정을 위해, direction 대신 Random.insideUnitCircle.normalized 사용
         base.SetSpawnInfo(owner, skill, Random.insideUnitCircle.normalized);
         transform.rotation = Quaternion.Euler(0f, 0f, Util.VectorToAngle(Random.insideUnitCircle.normalized));
-   
-        // 필요한 경우 여기에 추가 설정 코드 작성
     }
 
     protected override void Move()
@@ -39,7 +37,6 @@ public class Crocodile : Projectile
         // Skill 또는 SkillData가 null이면 이동을 중지
         if (Skill == null || Skill.SkillData == null)
         {
-            Debug.LogWarning("Skill or SkillData is null in Crocodile.Move");
             return; // 초기화되지 않았으므로 이동 처리를 중단
         }
         base.Move();
@@ -50,14 +47,13 @@ public class Crocodile : Projectile
         if (((1 << (int)Define.ELayer.Monster) & (1 << other.gameObject.layer)) != 0)
         {
             Monster monster = other.gameObject.GetComponent<Monster>();
+
             if (monster != null)
             {
-                Debug.Log("OnTriggerEnter2D called with " + other.gameObject.name);
-                Debug.Log("CollisionDetected Trigger Activated");
                 animator.SetTrigger("CollisionDetected"); // 충돌 애니메이션 실행
                 monster.OnDamaged(Owner, Skill); // 몬스터에게 데미지 적용
 
-                // 충돌 후 Crocodile 오브젝트 제거 로직
+                // 충돌 후 Crocodile 오브젝트 제거
                 StartCoroutine(DestroyAfterAnimation());
             }
         }
