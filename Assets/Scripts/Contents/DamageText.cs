ba6755ee052eee62cef30c0d0d956e9aaf6df484
kimjiheee <x_by_x@naver.com>
2024-06-16 04:42:04 +0900

    데미지 텍스트 수정
    - 텍스트 점점 투명해지게

--- Assets/Scripts/Contents/DamageText.cs
+++ Assets/Scripts/Contents/DamageText.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class DamageText : MonoBehaviour
 {
     private TextMeshPro textMesh;
+    Color alpha;
+    public float alphaSpeed= 0.5f;
     private float displayDuration = 0.7f; // �ؽ�Ʈ ǥ�� �ð�
     
     private float timer = 0f;   // �ؽ�Ʈ ǥ�� �ð� ������ ��������ϹǷ� �� �ð� ��� Ÿ�̸�
@@ -14,10 +16,14 @@ public class DamageText : MonoBehaviour
     void Awake()
     {
         textMesh = GetComponent<TextMeshPro>();
+        alpha = textMesh.color;
     }
 
     void Update()
     {
+        alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime * alphaSpeed); // 텍스트 알파값
+        textMesh.color = alpha;
+
             timer += Time.deltaTime; // ����� �ð��� �߰�
             if (timer >= displayDuration) // ��� �ð��� ǥ�� �ð��� �ʰ��ϸ�
             {
