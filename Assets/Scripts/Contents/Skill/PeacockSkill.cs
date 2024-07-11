6db00b6c80da8937dc83767f2ba04e1cec272695
kimjiheee <x_by_x@naver.com>
2024-07-11 18:53:52 +0900

    json 값 수정, 공작깃털 회전 버그 수정
    * 몬스터 속도 수정
    * 공작 깃털 속도, 쿨타임 수정
    * 공작 깃털 혼자 hero 따라서 회전하는 오류 -> 일단 쿠나이스킬 코드로 대체해놓음. 더 이상 회전은 안함

--- Assets/Scripts/Contents/Skill/PeacockSkill.cs
+++ Assets/Scripts/Contents/Skill/PeacockSkill.cs
@@ -5,50 +5,85 @@ using UnityEngine;
 using Data;
 public class PeacockSkill : SkillBase
 {
-	private List<Monster> monsterList = new List<Monster>();
+    public override void DoSkill()
+    {
+        Vector2 direction = Vector2.zero;
+        
+        Monster target = Managers.Object.FindClosestMonster(Owner.CenterPosition, 20);
+        if (target == null)
+        {
+            direction = Util.GetRandomDir();
+        }
+        else
+        {
+            direction = target.transform.position - Owner.CenterPosition;
+        }
 
-	public override void DoSkill()
-	{
-        if (BreakthroughHelper.Instance.CheckBreakthrough(SkillData.Index))
-            return;
+        AttackKunai(direction, 0);
 
-        monsterList.Clear();
-		GetTargets();
-	}
+        for (int i = 2; i <= SkillData.CastCount; ++i)
+        {
+            float angle = (i / 2) * SkillData.CastAngle;
+            if (i % 2 == 1)
+                angle *= -1;
+            AttackKunai(direction, angle);
+        }
+    }
 
-	private void GetTargets()
-	{
-		var list = Managers.Object.Monsters;
+    private void AttackKunai(Vector2 direction, float angle)
+    {
+        // Kunai proj = Managers.Object.Spawn<Kunai>(Owner.transform.position, SkillData.ProjectileNum);
+        // proj.SetSpawnInfo(Owner, this, Util.RotateVectorByAngle(direction, angle));
 
-		foreach (var monster in list)
-		{
-			if (monster.Hp <= 0)
-				continue;
+        Peacock peacock = Managers.Object.Spawn<Peacock>(Owner.transform.position, 1);
+        peacock.SetSpawnInfo(Owner, this, Util.RotateVectorByAngle(direction, angle));
+    }
 
-			if (Util.CheckTargetInScreen(monster.transform.position))
-			{
-				monsterList.Add(monster);
-			}
-		}
-		
-		for (int i = 0; i < SkillData.ProjectileNum; i++)
-		{
-			bool isNull = monsterList.Count == 0;
 
-			int idx = Random.Range(0, monsterList.Count);
-			Monster target = !isNull ? monsterList[idx] : null;
-			Peacock peacock = Managers.Object.Spawn<Peacock>(Owner.transform.position, 1);
+    // private List<Monster> monsterList = new List<Monster>();
 
-			peacock.SetTarget(target);
-			peacock.SetSpawnInfo(Owner, this, isNull ? Util.GetRandomDir() : Vector2.zero, false);
-	
-			if (!isNull)
-				monsterList.RemoveAt(idx);
-		}
-	}
+    // public override void DoSkill()
+    // {
+    //     if (BreakthroughHelper.Instance.CheckBreakthrough(SkillData.Index))
+    //         return;
 
-	public override void Clear()
-	{
+    //     monsterList.Clear();
+    //  GetTargets();
+    // }
 
-	}
+    // private void GetTargets()
+    // {
+    //  var list = Managers.Object.Monsters;
+
+    //  foreach (var monster in list)
+    //  {
+    //      if (monster.Hp <= 0)
+    //          continue;
+
+    //      if (Util.CheckTargetInScreen(monster.transform.position))
+    //      {
+    //          monsterList.Add(monster);
+    //      }
+    //  }
+        
+    //  for (int i = 0; i < SkillData.ProjectileNum; i++)
+    //  {
+    //      bool isNull = monsterList.Count == 0;
+
+    //      int idx = Random.Range(0, monsterList.Count);
+    //      Monster target = !isNull ? monsterList[idx] : null;
+    //      Peacock peacock = Managers.Object.Spawn<Peacock>(Owner.transform.position, 1);
+
+    //      peacock.SetTarget(target);
+    //      peacock.SetSpawnInfo(Owner, this, isNull ? Util.GetRandomDir() : Vector2.zero, false);
+    
+    //      if (!isNull)
+    //          monsterList.RemoveAt(idx);
+    //  }
+    // }
+
+    public override void Clear()
+    {
+
+    }
 }
