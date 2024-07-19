5009ad6a4252e1d416da8991d60a01b3a1087c60
JiHyeong-Hong <jirung003@gmail.com>
2024-07-20 01:35:40 +0900

    수정 - 앵무새 공전 버그

--- Assets/Scripts/Contents/Skill/Breakthrogh/AwakeningParrotSkill.cs
+++ Assets/Scripts/Contents/Skill/Breakthrogh/AwakeningParrotSkill.cs
@@ -1,4 +1,4 @@
-using DG.Tweening;
+﻿using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,7 @@ public class AwakeningParrotSkill : SkillBase
     private GameObject birdPool;
     private float orbitRadius = 1f;
     private List<Bird> birds = new List<Bird>();
+    private Sequence sequence; // 기존 시퀀스를 저장할 변수 추가
 
     public override void DoSkill()
     {
@@ -18,6 +19,7 @@ public class AwakeningParrotSkill : SkillBase
             {
                 birdPool = new GameObject("BirdPool");
                 birdPool.transform.parent = Owner.transform;
+                birdPool.transform.localPosition = Vector3.zero; // Owner의 위치로 이동
             }
             else
             {
@@ -51,11 +53,16 @@ public class AwakeningParrotSkill : SkillBase
 
     private void RotateSatellites()
     {
-        Sequence sequence = DOTween.Sequence();
+        // 기존 시퀀스가 존재하면 중지하고 삭제
+        if (sequence != null && sequence.IsActive())
+        {
+            sequence.Kill();
+
+        }
+        sequence = DOTween.Sequence();
         sequence.Append(DOTween.To(() => birdPool.transform.localRotation.eulerAngles,
                   x => birdPool.transform.localRotation = Quaternion.Euler(x),
-                  new Vector3(0, 0, 360 * SkillData.CastCount),
-                  3)
+                  new Vector3(0, 0, 360 * SkillData.CastCount), 6) // 6: 공전속도
              .SetEase(Ease.Linear))
             .AppendCallback(() =>
             {
--- Assets/Scripts/Contents/Skill/ParrotSkill.cs
+++ Assets/Scripts/Contents/Skill/ParrotSkill.cs
@@ -1,15 +1,17 @@
-using Cysharp.Threading.Tasks.Triggers;
+﻿using Cysharp.Threading.Tasks.Triggers;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Sequence = DG.Tweening.Sequence;
 
 public class ParrotSkill : SkillBase
 {
     private GameObject birdPool;
     private float orbitRadius = 1f;
     private List<Bird> birds = new List<Bird>();
-
+    private Sequence sequence; // 기존 시퀀스를 저장할 변수 추가
     public override void DoSkill()
     {
         ClearSatellites();
@@ -18,6 +20,7 @@ public class ParrotSkill : SkillBase
         {
             birdPool = new GameObject("BirdPool");
             birdPool.transform.parent = Owner.transform;
+            birdPool.transform.localPosition = Vector3.zero; // Owner의 위치로 이동
         }
 
         if (BreakthroughHelper.Instance.CheckBreakthrough(SkillData.Index))
@@ -48,12 +51,18 @@ public class ParrotSkill : SkillBase
 
     private void RotateSatellites()
     {
-        Sequence sequence = DOTween.Sequence();
+        // 기존 시퀀스가 존재하면 중지하고 삭제
+        if (sequence != null && sequence.IsActive())
+        {
+            sequence.Kill();
+
+        }
+        sequence = DOTween.Sequence();
         sequence.Append(DOTween.To(() => birdPool.transform.localRotation.eulerAngles,
                   x => birdPool.transform.localRotation = Quaternion.Euler(x),
-                  new Vector3(0, 0, 360 * SkillData.CastCount),
-                  3)
+                  new Vector3(0, 0, 360 * SkillData.CastCount), 7) // 7: 공전속도, 낮을수록 빠름
              .SetEase(Ease.Linear))
+            //.AppendInterval(1)      // 1초 동안 대기 후 다음 애니메이션 실행
             .AppendCallback(() =>
             {
                 ClearSatellites();
